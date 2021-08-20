b98a6875572fe6a622912e910c854b9b637ea552
Singh <h.singh8149@gmail.com>
2021-08-20 13:27:53 +0200

    fixes

--- MessageManager/MessageManager.py
+++ MessageManager/MessageManager.py
@@ -343,6 +343,8 @@ class MessageManager:
 
     def send_pabloMessage(self, message, bot):
         try:
+            sent = bot.reply_to(message, "Ah amore pablo!!")
+            time.sleep(4)
             xarray = ["❤", "💛", "💚", "💜"]
             i = 0
             for x in range(0, 15):
